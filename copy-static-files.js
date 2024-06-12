9707ca9ff061d8f8374051f9cd507049a37e016a
amirhosseinkhoshbin <khoshbinamirhosein@gmail.com>
2024-06-12 14:07:32 -0400

    Modified vit.config.js

--- copy-static-files.js
+++ copy-static-files.js
@@ -1,31 +1,31 @@
-const fs = require('fs-extra');
-const path = require('path');
+const fs = require("fs-extra");
+const path = require("path");
 
 const copyDirectoryContentsToRoot = (sourceDir, destDir) => {
-    if (fs.existsSync(sourceDir)) {
-        fs.readdirSync(sourceDir).forEach(item => {
-            const srcPath = path.join(sourceDir, item);
-            const destPath = path.join(destDir, item);
-            fs.copySync(srcPath, destPath);
-            console.log(`Copied ${srcPath} to ${destPath}`);
-        });
-    } else {
-        console.log(`Directory ${sourceDir} does not exist.`);
-    }
+  if (fs.existsSync(sourceDir)) {
+    fs.readdirSync(sourceDir).forEach((item) => {
+      const srcPath = path.join(sourceDir, item);
+      const destPath = path.join(destDir, item);
+      fs.copySync(srcPath, destPath);
+      console.log(`Copied ${srcPath} to ${destPath}`);
+    });
+  } else {
+    console.log(`Directory ${sourceDir} does not exist.`);
+  }
 };
 
 const main = () => {
-    const destDir = path.resolve(__dirname, 'dist');
+  const destDir = path.resolve(__dirname, "dist");
 
-    // Copy contents of 'static' to the root of 'dist'
-    const staticDir = path.resolve(__dirname, 'static');
-    copyDirectoryContentsToRoot(staticDir, destDir);
+  // Copy contents of 'static' to the root of 'dist'
+  const staticDir = path.resolve(__dirname, "static");
+  copyDirectoryContentsToRoot(staticDir, destDir);
 
-    // Copy 'webpage' directory to 'dist/webpage'
-    const webpageDir = path.resolve(__dirname, 'webpage');
-    const destWebpageDir = path.join(destDir, 'webpage');
-    fs.copySync(webpageDir, destWebpageDir);
-    console.log(`Copied ${webpageDir} to ${destWebpageDir}`);
+  // Copy 'webpage' directory to 'dist/webpage'
+  const webpageDir = path.resolve(__dirname, "webpage");
+  const destWebpageDir = path.join(destDir, "webpage");
+  fs.copySync(webpageDir, destWebpageDir);
+  console.log(`Copied ${webpageDir} to ${destWebpageDir}`);
 };
 
 main();
